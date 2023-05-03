706d9c1f7daf04b3b43345ad8a954e943492e8cd
neoseurae12 <87654809+neoseurae12@users.noreply.github.com>
2023-05-03 17:08:38 +0900

    입문: AOP ~ 다음으로 / 핵심-기본: 객체 지향 설계 & 스프링 ~ 스프링 핵심 원리 이해 1 - 예제 만들기

--- README.md
+++ README.md
@@ -1,20 +1,31 @@
-# Spring_Intro
+# Spring_Study
 
-GDSC 4-2 'Spring 입문 스터디' 학습 내용입니다.
+GDSC 4-2 'Spring 스터디' 학습 내용입니다.
 
-## 1. 프로젝트 환경설정
-## 2. 스프링 웹 개발 기초
-## 3. 회원 관리 예제 - 백엔드 개발
-## 4. 스프링 빈 & 의존관계
+## Spring-Intro
+
+### 1. 프로젝트 환경설정
+### 2. 스프링 웹 개발 기초
+### 3. 회원 관리 예제 - 백엔드 개발
+### 4. 스프링 빈 & 의존관계
 
 > - MY 정리본 : CH. 1 ~ 4 <br />
 >    https://velog.io/@neoseurae12/BESpring-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8-%ED%99%98%EA%B2%BD%EC%84%A4%EC%A0%95%EC%8A%A4%ED%94%84%EB%A7%81-%EC%9B%B9-%EA%B0%9C%EB%B0%9C-%EA%B8%B0%EC%B4%88%ED%9A%8C%EC%9B%90-%EA%B4%80%EB%A6%AC-%EC%98%88%EC%A0%9C-%EB%B0%B1%EC%97%94%EB%93%9C-%EA%B0%9C%EB%B0%9C%EC%8A%A4%ED%94%84%EB%A7%81-%EB%B9%88%EA%B3%BC-%EC%9D%98%EC%A1%B4%EA%B4%80%EA%B3%84
 
-## 5. 회원 관리 예제 - 웹 MVC 개발
-## 6. 스프링 DB 접근 기술
-## 7. AOP
+### 5. 회원 관리 예제 - 웹 MVC 개발
+### 6. 스프링 DB 접근 기술
+### 7. AOP
 
 > MY 정리본 : CH. 5 ~ 7 <br />
 > https://velog.io/@neoseurae12/BESpring-%ED%9A%8C%EC%9B%90-%EA%B4%80%EB%A6%AC-%EC%98%88%EC%A0%9C-%EC%9B%B9-MVC-%EA%B0%9C%EB%B0%9C%EC%8A%A4%ED%94%84%EB%A7%81-DB-%EC%A0%91%EA%B7%BC-%EA%B8%B0%EC%88%A0AOP#5-%ED%9A%8C%EC%9B%90-%EA%B4%80%EB%A6%AC-%EC%98%88%EC%A0%9C---%EC%9B%B9-mvc-%EA%B0%9C%EB%B0%9C
 
-## 8. 다음으로
+### 8. 다음으로
+
+## Spring-Core_Basic
+
+### 1. 객체 지향 설계 & 스프링
+### 2. 스프링 핵심 원리 이해 1 - 예제 만들기
+
+> MY 정리본 : CH. 5 ~ 7 <br />
+> https://velog.io/@neoseurae12/%ED%95%B5%EC%8B%AC-%EA%B8%B0%EB%B3%B8-%EA%B0%9D%EC%B2%B4-%EC%A7%80%ED%96%A5-%EC%84%A4%EA%B3%84-%EC%8A%A4%ED%94%84%EB%A7%81%EC%8A%A4%ED%94%84%EB%A7%81-%ED%95%B5%EC%8B%AC-%EC%9B%90%EB%A6%AC-%EC%9D%B4%ED%95%B4-1-%EC%98%88%EC%A0%9C-%EB%A7%8C%EB%93%A4%EA%B8%B0-4lxfjdbs
+
--- src/main/java/com/example/hellospring/SpringConfig.java
+++ src/main/java/com/example/hellospring/SpringConfig.java
@@ -1,13 +1,11 @@
 package com.example.hellospring;
 
+import com.example.hellospring.aop.TimeTraceAop;
 import com.example.hellospring.repository.*;
 import com.example.hellospring.service.MemberService;
-import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
@@ -19,23 +17,35 @@ public class SpringConfig {
 //        this.dataSource = dataSource;
 //    }
 
-    private EntityManager em;
+//    private EntityManager em;
+//
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
+//
+//        //return new MemoryMemberRepository();
+//        //return new JdbcMemberRepository(dataSource);
+//        //return new JdbcTemplateMemberRepository(dataSource);
+//        //return new JpaMemberRepository(em);
+//    }
 
-        //return new MemoryMemberRepository();
-        //return new JdbcMemberRepository(dataSource);
-        //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
 }
